30030f153f839970ae4c69457bb8442c3fe1d7da
xinmeng94 <xyaimao@gmail.com>
2022-10-03 18:41:50 +0200

    cambio Xin

--- Api.py
+++ Api.py
@@ -41,10 +41,30 @@ def reentrenar(data):
             pickle.dump(modelo, g)
     return
 
-@app.route("/prediccion", methods=['GET'])
-def prediccion(data):
-    with open("modelo", "rb") as f:
-        modelo = pickle.load(f)
-    pred = modelo.predict(data)
-    return pred
+@app.route('/predict', methods=['GET'])
+
+def predict():
+    model = pickle.load(open('data/model','rb'))
+
+    potential = float(request.args.get('potential', None))
+    finishing = float(request.args.get('finishing', None))
+    short_passing = float(request.args.get('short_passing', None))
+    volleys = float(request.args.get('volleys', None))
+    dribbling = float(request.args.get('dribbling', None))
+    long_passing = float(request.args.get('long_passing', None))
+    ball_control = float(request.args.get('ball_control', None))
+    reactions = float(request.args.get('reactions', None))
+    shot_power = float(request.args.get('shot_power', None))
+    long_shots = float(request.args.get('long_shots', None))
+    interceptions = float(request.args.get('interceptions', None))
+    positioning = float(request.args.get('positioning', None))
+    vision = float(request.args.get('vision', None))
+    standing_tackle = float(request.args.get('standing_tackle', None))
+    sliding_tackle = float(request.args.get('sliding_tackle', None))
+    
+    prediction = model.predict([[potential,finishing,short_passing,volleys,dribbling,long_passing,ball_control,reactions,
+                                shot_power,long_shots,interceptions,positioning,vision,standing_tackle,sliding_tackle]])
+    return render_template('predict.html', predict='La predicción es :  {}'.format(prediction))
+
+
 app.run()
