9965f0cad44b154dd45299c574317e2dff0f12ed
Toni <107318721+Toni2Morales@users.noreply.github.com>
2022-10-04 12:22:07 +0200

    def commit1

--- Api.py
+++ Api.py
@@ -1,23 +1,12 @@
-from flask import Flask, request, jsonify,render_template
+from flask import Flask, request, render_template
 import os
 import pickle
 from sklearn.metrics import mean_absolute_error as MAE
-
-
-
-def return_img_stream(img_local_path):
-   
-    import base64
-    img_stream = ''
-    with open(img_local_path, 'rb') as img_f:
-        img_stream = img_f.read()
-        img_stream = base64.b64encode(img_stream).decode()
-    return img_stream
-
-os.chdir(os.path.dirname(__file__))
-
+import pymysql
+import pandas as pd
+from os import environ
+# os.chdir(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -25,35 +14,59 @@ def hello():
     return render_template("index.html")
 
 
-@app.route("/monitorizar", methods=['GET'])
-def monitorizar(data, maeModelo):
-    x = data.drop("y")
-    y = data["y"]
-    with open("modelo", "rb") as f:
-        modelo = pickle.load(f)
-    score = MAE(y, modelo.predict(x))
-    if score > maeModelo:
-        reentrenar(data)
-    return 
-
-@app.route("/reentrenar", methods=['GET'])
-def reentrenar(data):
-    x = data.drop(columns = "Y")
-    y = data["Y"]
-    with open("modelo", "rb") as f:
+@app.route("/monitorizar", methods=['POST'])
+def monitorizar():
+    usuario = "admin"
+    contrasena = "789123456"
+    host = "dataeng.clev5oqi6ti6.us-east-2.rds.amazonaws.com"
+    conexion = pymysql.connect(user = usuario, password=contrasena, host = host, cursorclass = pymysql.cursors.DictCursor)
+    RDS = conexion.cursor()
+    RDS.execute("use players_database")
+    RDS.execute("select * from players_table")
+    data1 = pd.DataFrame(RDS.fetchall(), columns = ['potential', 'finishing', 'short_passing', 'volleys', 'dribbling',
+    'long_passing', 'ball_control', 'reactions', 'shot_power', 'long_shots','interceptions', 'positioning', 'vision', 'standing_tackle',
+    'sliding_tackle', 'overall_rating'])
+    data2 = pd.DataFrame(request.get_json(), columns = ['potential', 'finishing', 'short_passing', 'volleys', 'dribbling',
+    'long_passing', 'ball_control', 'reactions', 'shot_power', 'long_shots','interceptions', 'positioning', 'vision', 'standing_tackle',
+    'sliding_tackle', 'overall_rating']).astype("int")
+    x = data1.drop(columns = "overall_rating")
+    y = data1["overall_rating"]
+    with open("model", "rb") as f:
         modelo = pickle.load(f)
     score1 = MAE(y, modelo.predict(x))
-    modelo.fit(x, y)
-    score2 = MAE(y, modelo.predict(x))
-    if score2 < score1:
-        with open("modelo", "wb") as g:
+    x2 = data2.drop(columns = "overall_rating")
+    y2 = data2["overall_rating"]
+    score2 = MAE(y2, modelo.predict(x2))
+    result = ""
+    if score1 < score2:
+        result =  reentrenar(x, y, x2, y2)
+    return str(" la monitorización fue realizada")
+
+@app.route("/reentrenar", methods=['GET'])
+def reentrenar(X,Y, X2, Y2):
+    with open("model", "rb") as f:
+        modelo = pickle.load(f)
+    SCORE1 = MAE(Y2, modelo.predict(X2))
+    x3 = pd.concat([X, X2], ignore_index=True)
+    y3 = pd.concat([Y, Y2], ignore_index=True)
+    modelo.fit(x3, y3)
+    SCORE2 = MAE(Y2, modelo.predict(X2))
+    SiNo = ""
+    if SCORE2 < SCORE1:
+        SiNo = "El modelo ha sido reentrenado y "
+        with open("model", "wb") as g:
             pickle.dump(modelo, g)
-    return
+    return SiNo
 
 @app.route("/prediccion", methods=['GET'])
-def prediccion(data):
-    with open("modelo", "rb") as f:
+def prediccion():
+    data = pd.DataFrame(request.get_json(), columns = ['potential', 'finishing', 'short_passing', 'volleys', 'dribbling',
+    'long_passing', 'ball_control', 'reactions', 'shot_power', 'long_shots','interceptions', 'positioning', 'vision', 'standing_tackle',
+    'sliding_tackle']).astype("int")
+    with open("model", "rb") as f:
         modelo = pickle.load(f)
     pred = modelo.predict(data)
-    return pred
-app.run()
+    return "La predicción que ha realizado el modelo con los datos entregados es: " + str(pred)
+
+if __name__ == '__main__':
+  app.run(debug = True, host = '0.0.0.0', port=environ.get("PORT", 5000))
